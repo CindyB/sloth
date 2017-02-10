ba974268d8bfdebcd7ea0f3b1125728104bf864a
Cindy Dallaire <c.dallaire93@gmail.com>
2017-02-09 20:49:12 -0500

    Add user32 function in winutilities

--- Sloth/Sloth/Core/WinUtilities.cs
+++ Sloth/Sloth/Core/WinUtilities.cs
@@ -1,5 +1,7 @@
 ﻿using Sloth.Interfaces.Core;
 using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Sloth.Core
 {
@@ -29,6 +31,22 @@ namespace Sloth.Core
         {
             throw new NotImplementedException();
         }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDlgItem(IntPtr hDlg, int nIDDlgItem);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
     }
 
 }
