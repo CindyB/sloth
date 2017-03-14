603cf4262f3f7eefbb745454d23c03efce8d5701
Cindy Dallaire <c.dallaire93@gmail.com>
2017-03-13 22:03:51 -0400

    Bring back acceptances tests not working

---
+++ Sloth/AcceptanceTests/Learn/CatchAndLogRawEvent.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sloth.Core;
+using Sloth.Interfaces.Learn;
+using Sloth.Learn;
+using System;
+using System.IO;
+
+using System.Windows.Forms;
+
+using TestStack.BDDfy;
+
+namespace Sloth.AcceptanceTests.Learn
+{
+    [TestClass, Story(Title = "Catch a user event and log it raw",
+                      AsA = "Developer",
+           IWant = "To catch a raw user event",
+           SoThat = "I can log it")]
+    public class CatchAndLogRawEvent
+    {
+        private string logFileName = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + Path.DirectorySeparatorChar + "Sloth" + Path.DirectorySeparatorChar + "UserEvent.sloth";
+        private const string eventLine = "";
+        //private Button userButton;
+
+        //TODO Use message only windows
+
+        public void GivenUserButton(string buttonName)
+        {
+            //userButton = new Button();
+            //userButton.Name = buttonName;
+        }
+
+        public void GivenEventListenerService()
+        {
+            IEventListener eventListener = new EventListener(new ApplicationAdapter(), new ControlAdapter(), new Logger(), new WinUtilities());
+            eventListener.Start();
+        }
+
+        public void WhenButtonIsClick()
+        {
+            //userButton.PerformClick();
+        }
+
+        public void ThenEventIsLogInFile()
+        {
+            Assert.AreEqual(eventLine, File.ReadLines(logFileName));
+        }
+
+        [TestMethod]
+        public void CatchedEventLogged()
+        {
+            this.Given(x => x.GivenUserButton("MyTestButton"), "A user button named {0}")
+                .And(x => x.GivenEventListenerService(), "A event listener service")
+            .When(x => x.WhenButtonIsClick(), "When button is click")
+                .Then(x => x.ThenEventIsLogInFile(), "Then event is log in file")
+                .BDDfy();
+        }
+
+    }
+
+}
---
+++ Sloth/AcceptanceTests/Repeat/ReadAndRaiseFormattedEvent.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sloth.Interfaces.Learn;
+using Sloth.Interfaces.Repeat;
+using Sloth.Learn;
+using Sloth.Repeat;
+using System;
+using System.IO;
+
+using System.Reflection;
+
+using TestStack.BDDfy;
+
+namespace Sloth.AcceptanceTests.Automation
+{
+    [TestClass, Story(Title = "Read formatted user event and raise it",
+                      AsA = "Developer",
+           IWant = "To read a user event",
+           SoThat = "Raise it")]
+    public class ReadAndRaiseFormattedEvent
+    {
+        //TODO : Use windows message only 
+        private string m_logFileName = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + Path.DirectorySeparatorChar + "Sloth" + Path.DirectorySeparatorChar + "TestUserEvent.sloth";
+        private ILogger m_Logger;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            m_Logger = new Logger();
+            m_Logger.GetType().GetField("m_FilePath", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(m_Logger, m_logFileName);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            m_Logger.GetType().GetField("m_FilePath", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(m_Logger, null);
+            m_Logger = null;
+
+            File.Delete(m_logFileName);
+        }
+
+        public void GivenUserEventLogFileWithUserEvent(string eventLine)
+        {
+            m_Logger.Log(eventLine);
+        }
+
+        public void WhenReadAndRaiseFirstEvent()
+        {
+            IAutomaton auto = new Automaton();
+            auto.RepeatBehavior(m_logFileName);
+        }
+
+        public void ThenFirstEventReadIsRaised()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ReadEventRaised()
+        {
+            this.Given(x => x.GivenUserEventLogFileWithUserEvent("MyButton;Click"), "A user event log file with user event {0}")
+            .When(x => x.WhenReadAndRaiseFirstEvent(), "When read file and raise first event")
+                .Then(x => x.ThenFirstEventReadIsRaised(), "Then first user event read in file is raised")
+                .BDDfy();
+        }
+
+    }
+
+}
