8efe748f0ca56d67aa313b94c214a51bcacd0085
Cindy Dallaire <c.dallaire93@gmail.com>
2017-04-17 15:02:32 -0400

    Add not working example

---
+++ Sloth/Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using Sloth.Core;
+using Sloth.Learn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ISlothListener listener = new SlothListener(new ControlAdapter(), new Logger(), new WinUtilities());
+            listener.Start();
+
+            Application.Run(new Form1());
+        }
+    }
+}
